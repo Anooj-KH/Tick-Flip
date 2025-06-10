ea91d45103fd01595b87fccf25dda0de51e6dbd1
Anooj <anoojkh2002@gmail.com>
2025-06-10 21:27:13 +0530

    hour section updated

--- src/components/FlipClock.jsx
+++ src/components/FlipClock.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import FlipUnit from "./FlipUnit"; 
+import FlipUnit from "./FlipUnit";
 
 
 const FlipClock = () => {
@@ -12,8 +12,9 @@ const FlipClock = () => {
     }, []);
 
     const formatNumber = (num) => String(num).padStart(2, "0");
-
+    const [is24Hour, setIs24Hour] = useState(false);
     const hours = formatNumber(time.getHours() % 12 || 12);
+    const fullHours = formatNumber(time.getHours());
     const minutes = formatNumber(time.getMinutes());
     const seconds = formatNumber(time.getSeconds());
     const ampm = time.getHours() >= 12 ? "PM" : "AM";
@@ -26,9 +27,12 @@ const FlipClock = () => {
             {/* Center Stripe */}
             <div className="absolute top-[52%] left-0 w-full h-1 bg-black opacity-60 z-100" />
             {/* Hour Block */}
-            <div className="bg-[#1a1a1a] rounded-3xl px-6 py-4 mr-16 flex flex-col items-center shadow-lg">
-                <div className="text-white text-xl font-semibold mb-1">{dateStr}</div>
-                <FlipUnit value={hours} labelLeft={ampm} />
+            <div
+                className="bg-[#1a1a1a] rounded-3xl px-6 py-4 mr-16 flex flex-col items-center shadow-lg cursor-pointer"
+                onClick={() => setIs24Hour(prev => !prev)}
+            >
+                <div className="text-white text-lg font-semibold mb-1">{dateStr}</div>
+                <FlipUnit value={is24Hour ? fullHours : hours} labelLeft={!is24Hour ? ampm : ""} />
             </div>
 
             {/* Minute Block */}
